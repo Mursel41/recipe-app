dbd55530ef11b897bb7778d3834dfd1db2be6923
Mursel Genc <murselgenc41@gmail.com>
2020-10-19 18:33:20 +0200

    Updates

---
+++ precache-manifest.9792fea79205b022a78e19c5bead7097.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a8704b93ebd257a322b2d1190d0de59",
+    "url": "/recipe-app/index.html"
+  },
+  {
+    "revision": "855bf5a4d4d2197c36df",
+    "url": "/recipe-app/static/css/main.df93593f.chunk.css"
+  },
+  {
+    "revision": "9d180c7a817601a120c9",
+    "url": "/recipe-app/static/js/2.fe882f49.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/recipe-app/static/js/2.fe882f49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "855bf5a4d4d2197c36df",
+    "url": "/recipe-app/static/js/main.5f41f93c.chunk.js"
+  },
+  {
+    "revision": "0fda6634f0477e5c9a25",
+    "url": "/recipe-app/static/js/runtime-main.85ba4b61.js"
+  }
+]);
